3fcbdf1b87ef6f8887500b0708d9e09c16954e12
Alexander Andreev <andreev-alef@users.noreply.github.com>
2020-09-28 11:03:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$form = ActiveForm::begin([
+            'id' => 'filter',
+            'options' => ['class' => 'form-horizontal'],
+        ]);
+/* @var $this yii\web\View */
+$this->title = 'НМЦ';
+?>
+<div class="site-index">
+
+    <!--    <div class="jumbotron">
+            <h1>Научно-методический центр</h1>
+    
+            <p class="lead">Локальный сайт</p>
+    
+            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        </div>
+    -->
+    <div class="body-content">
+
+        <div class="row">
+            <!-- <div class="col-lg-4"> -->
+            <h2>
+                База удостоверений
+            </h2>
+            <p>Всего записей: <?= $countNotEmpty ?></p>
+<?php $n = count($data); ?>
+            <table class="table table-hover">
+                <tbody>
+<?php for ($i = $n - 1; $i > 0; $i--): ?>
+                        <tr>
+                            <td class="align-middle"><?= $data[$i][4] ?></td>
+                            <td class="align-middle"><?= $data[$i][5] ?></td>
+                            <td class="align-middle"><?= $data[$i][6] ?></td>
+                            <td class="align-middle"><?= $data[$i][8] ?></td>
+                            <td class="align-middle"><?= $data[$i][9] ?></td>
+                            <td class="align-middle"><?= $data[$i][10] ?></td>
+                            <td class="align-middle"><?= $data[$i][11] ?></td>
+                            <td class="align-middle"><?= $data[$i][12] ?></td>
+                            <td class="align-middle"><?= $data[$i][13] ?></td>
+                        </tr>
+<?php endfor; ?>
+                </tbody>
+            </table>
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        </div>
+        <!-- <div class="col-lg-4">
+            <h2>Heading</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Heading</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        </div> -->
+    </div>
+
+</div>
+</div>
