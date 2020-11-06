903ed3d17d4be6db8649a912c62a13e7114c8628
alef <andreev.alef@gmail.com>
2020-11-07 00:16:20 +0700

    Простой поиск вместо regexp. Поиск по фамилии

---
+++ test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $str = 'Казарезова';
+        $str_search = ' ';
+        ?>
+
+        <p>Оригинальная строка «<?= $str; ?>»</p>
+        <?php
+        mb_regex_encoding('UTF-8');
+        $m = mb_stripos($str, $str_search)!== false;
+        ?>
+        <p>Искомая строка «<?= $str_search; ?>»</p>
+        <p>Кол-во: «<= var_dump($matches); ?>»</p>
+        <p><?= var_dump($m) ?></p>
+
+    </body>
+</html>
