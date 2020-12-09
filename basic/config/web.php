02981d65f157f4d0ace0deefcfabf9ffd72c734f
typograph\typo <andreev.alef@gmail.com>
2020-12-09 18:01:41 +0700

    Начало работы с приложением «Аттестация руководителей»

--- basic/config/web.php
+++ basic/config/web.php
@@ -4,6 +4,7 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    'defaultRoute' => 'site/index',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
@@ -11,6 +12,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
