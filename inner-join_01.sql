8527307d4d60975e4064282414752d6973ac32c0
typograph\typo <andreev.alef@gmail.com>
2021-02-09 18:08:54 +0700

    Создание личной карточки пользователя

---
+++ inner-join_01.sql
@@ -0,0 +1,13 @@
+SELECT nmc42test.nmc42mdl_user.lastname,
+	nmc42test.nmc42mdl_user.firstname,
+	DATE_FORMAT(FROM_UNIXTIME(nmc42test.nmc42mdl_grade_grades.timemodified), '%d.%m.%Y') AS date,
+	nmc42test.nmc42mdl_course.fullname -- ,
+--	COUNT(nmc42test.nmc42mdl_course.fullname) AS N
+FROM nmc42test.nmc42mdl_grade_grades
+INNER JOIN nmc42test.nmc42mdl_user ON nmc42test.nmc42mdl_user.id = nmc42test.nmc42mdl_grade_grades.userid
+INNER JOIN nmc42test.nmc42mdl_grade_items ON nmc42test.nmc42mdl_grade_items.id = nmc42test.nmc42mdl_grade_grades.itemid
+INNER JOIN nmc42test.nmc42mdl_course ON nmc42test.nmc42mdl_course.id = nmc42test.nmc42mdl_grade_items.courseid
+WHERE nmc42test.nmc42mdl_grade_items.itemname IS NOT NULL
+	AND nmc42test.nmc42mdl_grade_grades.finalgrade IS NOT NULL 
+	AND nmc42test.nmc42mdl_user.id = 124
+-- GROUP BY nmc42test.nmc42mdl_course.fullname, date;
